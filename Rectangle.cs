3bfa196708cd7a35a348b5e12e2098c57d5ea5f0
Yatripatel17 <yatripatel1710@gmail.com>
2025-02-25 13:59:52 -0500

    Initialized git and added class Rectangle.

---
+++ Rectangle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+	public class Rectangle
+	{
+		public int length;
+		public int width;
+		public int max_value = 1200;
+
+		public Rectangle()
+		{
+			this.length = 1;
+			this.width = 1;
+		}
+
+		public Rectangle(int length, int width)
+		{
+			this.length = length;
+			this.width = width;
+		}
+
+		public int GetLength()
+		{
+			return this.length;
+		}
+		public int SetLength(int length)
+		{
+			if (length < max_value)
+			{
+				this.length = length;
+				return this.length;
+			}
+			else
+			{
+				Console.WriteLine("Invalid input. Length must be less than 1200");
+			}
+			return this.length;
+		}
+
+		public int GetWidth()
+		{
+			return this.width;
+		}
+
+		public int SetWidth(int width)
+		{
+			if (width < max_value)
+			{
+				this.width = width;
+				return this.width;
+			}
+			else
+			{
+				Console.WriteLine("Invalid input. Width must be less than 1200.");
+			}
+			return this.width;
+		}
+		public int GetPerimeter()
+		{
+			return 2 * (this.length + this.width);
+		}
+		public int GetArea()
+		{
+			return (this.length * this.width);
+		}
+	}
+}
