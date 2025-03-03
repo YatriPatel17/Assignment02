f56bd580a7c81876db3a62032847b4df622d56e7
Yatripatel17 <yatripatel1710@gmail.com>
2025-03-03 18:51:51 -0500

    Updated test-case names

--- Assignment02.Tests/RectangleTest.cs
+++ Assignment02.Tests/RectangleTest.cs
@@ -11,6 +11,11 @@ namespace Assignment02.Tests
 	
 	public class RectangleTest
 	{
+		/// <summary>
+		/// This test verifies the behavior of the GetLength.
+		/// It ensure that default length is 1.
+		/// I chose this test to confirm that the Rectangle class initializes ite length correctly.
+		/// </summary>
 		[Test]
 		public void GetLength_InitialValue_Returns1()
 		{
@@ -25,6 +30,12 @@ namespace Assignment02.Tests
 			Assert.That(expected, Is.EqualTo(actual));
 		}
 
+		/// <summary>
+		/// This test verifies the behavior of the GetLength after setting the length to 20.
+		/// This test ensure that the updated length correctly returns in the method.
+		/// I chose this test to verify that the SetLength method will properly update and retrieves the updated length.
+		/// This is an important test because it confirms that updated value can be set and retrieves properly.
+		/// </summary>
 		[Test]
 		public void GetLength_AfterSettingLength_Returns20()
 		{
@@ -40,6 +51,12 @@ namespace Assignment02.Tests
 			Assert.That(expected, Is.EqualTo(actual));
 		}
 
+		/// <summary>
+		/// This test verifies the behavior of the GetLength after setting the length to 30.
+		/// This test ensure that the updated length correctly returns in the method.
+		/// I chose this test to verify that the SetLength method will properly update and retrieves the updated length.
+		/// This is an important test because it confirms that updated value can be set and retrieves properly.
+		/// </summary>
 		[Test]
 		public void GetLength_AfterSettingLength_Returns30()
 		{
@@ -55,6 +72,12 @@ namespace Assignment02.Tests
 			Assert.That(expected, Is.EqualTo(actual));
 		}
 
+		/// <summary>
+		/// This test verifies the behavior of the GetLength after setting the length to 15.
+		/// This test ensure that the updated length correctly returns in the method.
+		/// I chose this test to verify that the SetLength method will properly update and retrieves the updated length.
+		/// This is an important test because it confirms that updated value can be set and retrieves properly.
+		/// </summary>
 		[Test]
 		public void SetLength_AfterSettingLength_Returns15()
 		{
@@ -68,6 +91,13 @@ namespace Assignment02.Tests
 			// Assert
 			Assert.That(expected, Is.EqualTo(actual));
 		}
+
+		/// <summary>
+		/// This test verifies the behavior of the SetLength method by setting the length to 1199.
+		/// This test ensure that the method correctly updates and returns the new length.
+		/// I chose this test to verify that the SetLength method can handle and correctly store large valid value.
+		/// This is an important test because it confirms that the Rectangle class support a large range of valid inputs.
+		/// </summary>
 
 		[Test]
 		public void SetLength_AfterSettingLength_Returns1199()
@@ -83,6 +113,12 @@ namespace Assignment02.Tests
 			Assert.That(expected, Is.EqualTo(actual));
 		}
 
+		/// <summary>
+		/// This test verifies the behavior of the SetLength method by setting the invalid length to 1201.
+		/// This test ensure that the method correctly returns -1 when we provide out of range length.
+		/// I chose this test to verify that the SetLength method properly reject the invalid input value.
+		/// This is an important test because it confirms that the Rectangle class enforces its length constraints.
+		/// </summary>
 		[Test]
 		public void SetLength_AfterSettingLength_Returns1201()
 		{
@@ -97,6 +133,11 @@ namespace Assignment02.Tests
 			Assert.That(expected, Is.EqualTo(actual));
 		}
 
+		/// <summary>
+		/// This test verifies the behavior of the GetWidth.
+		/// It ensure that default width is 1.
+		/// I chose this test to confirm that the Rectangle class initializes ite width correctly.
+		/// </summary>
 		[Test]
 		public void GetWidth_InitialValue_Returns1()
 		{
@@ -111,8 +152,14 @@ namespace Assignment02.Tests
 			Assert.That(expected, Is.EqualTo(actual));
 		}
 
+		/// <summary>
+		/// This test verifies the behavior of the GetWidth after setting the width to 12.
+		/// This test ensure that the updated width correctly returns in the method.
+		/// I chose this test to verify that the SetWidth method will properly update and retrieves the updated width.
+		/// This is an important test because it confirms that updated value can be set and retrieves properly.
+		/// </summary>
 		[Test]
-		public void GetWidth_AfterSettingLength_Returns12()
+		public void GetWidth_AfterSettingWidth_Returns12()
 		{
 			// Arrange
 			Rectangle rect = new Rectangle();
@@ -126,8 +173,14 @@ namespace Assignment02.Tests
 			Assert.That(expected, Is.EqualTo(actual));
 		}
 
+		/// <summary>
+		/// This test verifies the behavior of the GetWidth after setting the width to 34.
+		/// This test ensure that the updated width correctly returns in the method.
+		/// I chose this test to verify that the SetWidth method will properly update and retrieves the updated width.
+		/// This is an important test because it confirms that updated value can be set and retrieves properly.
+		/// </summary>
 		[Test]
-		public void GetWidth_AfterSettingLength_Returns34()
+		public void GetWidth_AfterSettingWidth_Returns34()
 		{
 			// Arrange
 			Rectangle rect = new Rectangle();
@@ -140,5 +193,6 @@ namespace Assignment02.Tests
 			// Assert
 			Assert.That(expected, Is.EqualTo(actual));
 		}
+
 	}
 }
