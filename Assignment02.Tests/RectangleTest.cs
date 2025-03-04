f8e9fe835a31c1f27fa0c25ff2db31999af3d97f
Kunj Lakhani <Klakhani0551@conestogac.on.ca>
2025-03-03 19:03:15 -0500

    Added 9 more test-cases

--- Assignment02.Tests/RectangleTest.cs
+++ Assignment02.Tests/RectangleTest.cs
@@ -194,5 +194,182 @@ namespace Assignment02.Tests
 			Assert.That(expected, Is.EqualTo(actual));
 		}
 
-	}
+        /// <summary>
+        /// This test verifies the behavior of the GetWidth after setting the width to 22.
+        /// This test ensure that the updated width correctly returns in the method.
+        /// I chose this test to verify that the SetWidth method will properly update and retrieves the updated width.
+        /// This is an important test because it confirms that updated value can be set and retrieves properly.
+        /// </summary>
+        [Test]
+        public void SetWidth_AfterSettingWidth_Returns22()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+
+            // Act
+            int actual = rect.SetWidth(22);
+            int expected = 22;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        /// <summary>
+        /// This test verifies the behavior of the SetWidth method by setting the width to 1199.
+        /// This test ensure that the method correctly updates and returns the new width.
+        /// I chose this test to verify that the SetWidth method can handle and correctly store large valid value.
+        /// This is an important test because it confirms that the Rectangle class support a large range of valid inputs.
+        /// </summary>
+        [Test]
+        public void SetWidth_AfterSettingWidth_Returns1199()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+
+            // Act
+            int actual = rect.SetWidth(1199);
+            int expected = 1199;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        /// <summary>
+        /// This test verifies the behavior of the SetWidth method by setting the invalid length to 1208.
+        /// This test ensure that the method correctly returns -1 when we provide out of range width.
+        /// I chose this test to verify that the SetWidth method properly reject the invalid input value.
+        /// This is an important test because it confirms that the Rectangle class enforces its width constraints.
+        /// </summary>
+        [Test]
+        public void SetWidth_AfterSettingWidth_Returns1208()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+            int actual = rect.SetWidth(1208);
+
+            // Act
+            int expected = -1;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        /// <summary>
+        /// This test verifies the behavior of the GetPerimeter after setting the length and width values (4, 7).
+        /// This test ensure that the updated values correctly returns in the method.
+        /// I chose this test to verify that the GetPerimeter method properly computes the perimeter using valid input values.
+        /// This is an important test because it confirms that the calculation of perimeter is correct.
+        /// </summary>
+        [Test]
+        public void GetPerimeter_ValidValues_ReturnsCorrectPerimeter()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle(4, 7);
+
+            // Act
+            int actual = rect.GetPerimeter();
+            int expected = 22;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        /// <summary>
+        /// This test verifies the behavior of the GetPerimeter method.
+        /// It ensure that default values length is 1 and width is 1.
+        /// I chose this test to confirm that the Rectangle class initializes with correct default values.
+        /// </summary>
+        [Test]
+        public void GetPerimeter_DefaultValues_ReturnsCorrectPerimeter()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+
+            // Act
+            int actual = rect.GetPerimeter();
+            int expected = 4;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        /// <summary>
+        /// This test verifies the behavior of the GetPerimeter after setting the maximum valid length and width values (1200, 1200).
+        /// This test ensure that the updated large values correctly returns in the method.
+        /// I chose this test to verify that the GetPerimeter method properly computes the perimeter using valid large input values.
+        /// This is an important test because it confirms that the calculation of perimeter is correct.
+        /// </summary>
+        [Test]
+        public void GetPerimeter_MaxValues_ReturnsCorrectPerimeter()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle(1200, 1200);
+            int actual = rect.GetPerimeter();
+
+            // Act
+            int expected = 4800;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        /// <summary>
+        /// This test verifies the behavior of the GetArea after setting the length and width values (2, 2).
+        /// This test ensure that the updated values correctly returns in the method.
+        /// I chose this test to verify that the GetArea method properly computes the perimeter using valid input values.
+        /// This is an important test because it confirms that the calculation of area is correct.
+        /// </summary>
+        [Test]
+        public void GetArea_ValidValues_ReturnsCorrectArea()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle(2, 2);
+
+            // Act
+            int actual = rect.GetArea();
+            int expected = 4;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        /// <summary>
+        /// This test verifies the behavior of the GetArea method.
+        /// It ensure that default values length is 1 and width is 1.
+        /// I chose this test to confirm that the Rectangle class initializes with correct default values.
+        /// </summary>
+        [Test]
+        public void GetArea_DefaultValues_ReturnsCorrectArea()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle();
+
+            // Act
+            int actual = rect.GetArea();
+            int expected = 1;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        /// <summary>
+        /// This test verifies the behavior of the GetArea after setting the maximum valid length and width values (1200, 1200).
+        /// This test ensure that the updated large values correctly returns in the method.
+        /// I chose this test to verify that the GetArea method properly computes the perimeter using valid large input values.
+        /// This is an important test because it confirms that the calculation of perimeter is correct.
+        /// </summary>
+        [Test]
+        public void GetArea_MaxValues_ReturnsCorrectArea()
+        {
+            // Arrange
+            Rectangle rect = new Rectangle(1200, 1200);
+            int actual = rect.GetArea();
+
+            // Act
+            int expected = 1440000;
+
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+    }
 }
